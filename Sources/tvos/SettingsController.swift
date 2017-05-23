dc746096c16da7b2e41c5f08eedaea92a6d4bbe2
Alexander Shvets <alexander.shvets@gmail.com>
2017-05-23 19:05:13 -0400

    fix lint

--- Sources/tvos/SettingsController.swift
+++ Sources/tvos/SettingsController.swift
@@ -51,7 +51,7 @@ class SettingsController: GidOnlineBaseCollectionViewController {
 
     let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
 
-    let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+    let okAction = UIAlertAction(title: "OK", style: .default) {
       let history = (self.adapter as! GidOnlineServiceAdapter).history
 
       history.clear()
@@ -72,7 +72,7 @@ class SettingsController: GidOnlineBaseCollectionViewController {
 
     let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
 
-    let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+    let okAction = UIAlertAction(title: "OK", style: .default) {
       let bookmarks = (self.adapter as! GidOnlineServiceAdapter).bookmarks
 
       bookmarks.clear()
